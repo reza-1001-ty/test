abf585679de1f60f0b60793a44de134502ca6826
reza <153919816+reza-1001-ty@users.noreply.github.com>
2026-05-05 17:34:15 +0330

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,10 +1,20 @@
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
+import ccxt
 
-url = "https://youtu.be/NXjnPUEWcmM?si=M2pQbskVTNrzb-Mo"
+exchange = ccxt.binance()
+symbol = 'BTC/USDT'
+timeframe = '5m'
 
-yt = YouTube(url, "WEB", on_progress_callback=on_progress)
-print(yt.title)
+# گرفتن ۲ کندل آخر
+candles = exchange.fetch_ohlcv(symbol, timeframe, limit=2)
 
-ys = yt.streams.get_highest_resolution()
-ys.download()
+current_candle = candles[-1] 
+timestamp, open_, high, low, close, volume = current_candle
+
+# چک می‌کنیم که این کندل هنوز بسته نشده؟
+import time
+now = exchange.milliseconds()
+candle_duration = 5 * 60 * 1000  # ۵ دقیقه به میلی‌ثانیه
+if now - timestamp < candle_duration:
+    print("کندل جاری پیدا شد:", current_candle)
+else:
+    print("کندل بسته شده، باید منتظر کندل بعدی بمونیم یا با ticker بسازیمش")
